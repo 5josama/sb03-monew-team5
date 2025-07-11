8c322157a9ac559b757bce5928530e3ee2e14e8f
Kangmoongu <sprtms5325@naver.com>
2025-07-11 09:42:58 +0900

    test: CommentControllerTest의 댓글등록 성공 테스트 작성(red)

---
+++ src/test/java/com/sprint5team/monew/domain/comment/controller/CommentControllerTest.java
@@ -0,0 +1,73 @@
+package com.sprint5team.monew.domain.comment.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sprint5team.monew.domain.comment.dto.CommentDto;
+import com.sprint5team.monew.domain.comment.dto.CommentRegisterRequest;
+import com.sprint5team.monew.domain.comment.service.CommentService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import static org.awaitility.Awaitility.given;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CommentController.class)
+public class CommentControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private CommentService commentService;
+
+    @Test
+    public void 댓글등록_성공_테스트() throws Exception {
+        //Given
+        UUID commentId = UUID.randomUUID();
+        UUID articleId = UUID.randomUUID();
+        UUID userId = UUID.randomUUID();
+
+        CommentRegisterRequest commentRegisterRequest = new CommentRegisterRequest(articleId, userId, "테스트 댓글 입니다.");
+        CommentDto createdComment = new CommentDto(
+                commentId,
+                articleId,
+                userId,
+                "UserNickname",
+                commentRegisterRequest.content(),
+                (long) 0,
+                false,
+                Instant.now()
+        );
+
+        given(commentService.create(any(CommentRegisterRequest.class))).willReturn(createdComment);
+
+        //When && Then
+        mockMvc.perform(post("api/comments")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(commentRegisterRequest)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(commentId.toString()))
+                .andExpect(jsonPath("$.articleId").value(articleId.toString()))
+                .andExpect(jsonPath("$.userId").value(articleId.toString()))
+                .andExpect(jsonPath("$.userNickName").value("UserNickname"))
+                .andExpect(jsonPath("$.content").value("테스트 댓글 입니다."))
+                .andExpect(jsonPath("$.likeCount").value(0))
+                .andExpect(jsonPath("$.likedByMe").value(false));
+
+    }
+
+
+}
