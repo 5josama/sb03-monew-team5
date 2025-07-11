9a76d372d5f3719ca7db6ab3ee7afa4975a0c783
kangho1870 <ho_gang@naver.com>
2025-07-11 10:59:28 +0900

    test: ArticleControllerTest 뉴스 기사 view POST API 요청 테스트 코드 추가 (red) #11

---
+++ src/test/java/com/sprint5team/monew/controller/article/ArticleControllerTest.java
@@ -0,0 +1,61 @@
+package com.sprint5team.monew.controller.article;
+
+import com.sprint5team.monew.domain.article.dto.ArticleViewDto;
+import com.sprint5team.monew.domain.article.service.ArticleService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@WebMvcTest(ArticleController.class)
+@DisplayName("ArticleController 단위 테스트")
+public class ArticleControllerTest {
+
+    @Autowired private MockMvc mockMvc;
+
+    @MockitoBean private ArticleService articleService;
+
+    @Test
+    void 뉴스기사_VIEW_생성_API가_정상적으로_동작한다() throws Exception {
+        // given
+        UUID articleId = UUID.randomUUID();
+        UUID userId = UUID.randomUUID();
+
+        ArticleViewDto articleViewDto = new ArticleViewDto(
+                UUID.randomUUID(),
+                userId,
+                Instant.now(),
+                articleId,
+                "NAVER",
+                "https://naver.com/news/12333",
+                "test",
+                Instant.now(),
+                "요약",
+                10,
+                10
+        );
+
+        given(articleService.saveArticleView(articleId, userId)).willReturn(articleViewDto);
+
+        // when & then
+        mockMvc.perform(post("/api/articles/{articleId}/article-views", articleId)
+                .header("MoNew-Request-User-ID", userId.toString())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.articleId").value(articleId.toString()))
+                .andExpect(jsonPath("$.viewedBy").value(userId.toString()));
+    }
+}
