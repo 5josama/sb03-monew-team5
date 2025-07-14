66aa366d22ea26622872f3913715f2ffe6b320f4
kangho1870 <ho_gang@naver.com>
2025-07-15 01:09:58 +0900

    feat: ArticleRepositoryTest 뉴스 기사 목록 QueryDSL 커서기반 페이징 조회 로직 구현 (green) #26

---
+++ src/main/java/com/sprint5team/monew/domain/article/repository/ArticleCustomRepository.java
@@ -0,0 +1,11 @@
+package com.sprint5team.monew.domain.article.repository;
+
+import com.sprint5team.monew.domain.article.dto.CursorPageFilter;
+import com.sprint5team.monew.domain.article.entity.Article;
+
+import java.util.List;
+
+public interface ArticleCustomRepository {
+
+    List<Article> findByCursorFilter(CursorPageFilter filter, List<String> interestKeyword);
+}
