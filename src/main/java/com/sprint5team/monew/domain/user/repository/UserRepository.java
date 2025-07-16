0ce1a3f35956b755d9c43d1a3251a6618bbbcf16
JinsolPark <jinsol8k@gmail.com>
2025-07-16 09:44:39 +0900

    refactor: 리뷰 피드백 반영

--- src/main/java/com/sprint5team/monew/domain/user/repository/UserRepository.java
+++ src/main/java/com/sprint5team/monew/domain/user/repository/UserRepository.java
@@ -10,5 +10,5 @@ public interface UserRepository extends JpaRepository<User, UUID> {
   Optional<User> findByEmail(String email);
   boolean existsByEmail(String email);
 
-  User findByEmailAndPassword(String email, String password);
+  Optional<User> findByEmailAndPassword(String email, String password);
 }
