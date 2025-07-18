e4c5e5a2ff3a323a5ca03633a8b89288f87efc7e
JinsolPark <jinsol8k@gmail.com>
2025-07-18 17:05:54 +0900

    feat: 사용자 활동 내역 조회 구현 완료(green)

--- src/test/java/com/sprint5team/monew/integration/user/UserActivityIntegrationTest.java
+++ src/test/java/com/sprint5team/monew/integration/user/UserActivityIntegrationTest.java
@@ -1,6 +1,5 @@
 package com.sprint5team.monew.integration.user;
 
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -32,11 +31,8 @@ import com.sprint5team.monew.domain.user_interest.mapper.UserInterestMapper;
 import com.sprint5team.monew.domain.user_interest.repository.UserInterestRepository;
 import jakarta.persistence.EntityManager;
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -56,76 +52,109 @@ public class UserActivityIntegrationTest {
   @Autowired
   private UserActivityServiceImpl userActivityService;
 
+  @Autowired private UserRepository userRepository;
+  @Autowired private InterestRepository interestRepository;
+  @Autowired private KeywordRepository keywordRepository;
+  @Autowired private UserInterestRepository userInterestRepository;
+  @Autowired private CommentRepository commentRepository;
+  @Autowired private LikeRepository likeRepository;
+  @Autowired private ArticleRepository articleRepository;
+  @Autowired private ArticleCountRepository articleCountRepository;
+  @Autowired private CommentMapper commentMapper;
+  @Autowired private ArticleViewMapper articleViewMapper;
+  @Autowired private UserInterestMapper userInterestMapper;
+
   @Autowired
   private EntityManager entityManager;
-
-  @Mock private UserRepository userRepository;
-  @Mock private ArticleRepository articleRepository;
-  @Mock private ArticleCountRepository articleCountRepository;
-  @Mock private ArticleViewMapper articleViewMapper;
-  @Mock private InterestRepository interestRepository;
-  @Mock private KeywordRepository keywordRepository;
-  @Mock private UserInterestRepository userInterestRepository;
-  @Mock private UserInterestMapper userInterestMapper;
-  @Mock private CommentRepository commentRepository;
-  @Mock private LikeRepository likeRepository;
-  @Mock private CommentMapper commentMapper;
 
   @Test
   void 사용자_활동_조회_API_통합_테스트() throws Exception {
     // given
-    User user = userRepository.save(new User("test@test.kr","test","test1234"));
-    User otherUser = userRepository.save(new User("other@test.kr","other","test1234"));
-
-    Article article = articleRepository.save(new Article("Naver","http://www.nspna.com/news/?mode=view&newsid=42872","뉴스 제목","요약",Instant.now()));
-    ArticleCount articleCount = articleCountRepository.save(new ArticleCount(article, user));
-    ArticleViewDto articleViewDto = articleViewMapper.toDto(article, user, articleCount);
-
-    Interest interest = interestRepository.save(new Interest(Instant.now(), "test", 2L));
-    keywordRepository.save(new Keyword(Instant.now(), "축구", interest));
-    keywordRepository.save(new Keyword(Instant.now(), "야구", interest));
-    UserInterest userInterest = userInterestRepository.save(new UserInterest(Instant.now(), user, interest));
-    SubscriptionDto subscriptionDto = userInterestMapper.toDto(userInterest);
-
-    Comment comment = commentRepository.save(new Comment(article, user, "댓글 내용 입니다."));
-    CommentDto commentDto = commentMapper.toDto(comment);
-
-    Comment commentFromOther =  commentRepository.save(new Comment(article, otherUser, "다른 사람이 단 댓글 내용입니다."));
-
-    Like like = likeRepository.save(new Like(commentFromOther, user));
-    CommentLikeDto commentLikeDto = commentMapper.toDto(like);
-
+    User user = User.builder()
+        .email("test@test.kr")
+        .nickname("test")
+        .password("test1234")
+        .isDeleted(false)
+        .build();
+    userRepository.save(user);
     entityManager.flush();
+
+    Article article = new Article("NAVER", "https://naver.com/news/12333", "기사제목", "요약", Instant.now());
+    articleRepository.save(article);
+    entityManager.flush();
+
+    ArticleCount articleCount = new ArticleCount(article, user);
+    articleCountRepository.save(articleCount);
+    entityManager.flush();
+
+    Comment comment = new Comment(article, user, "댓글 내용 입니다.");
+    commentRepository.save(comment);
+    entityManager.flush();
+
+    User commentUser = User.builder()
+        .email("comment@test.kr")
+        .nickname("commentUser")
+        .password("test1234")
+        .isDeleted(false)
+        .build();
+    userRepository.save(commentUser);
+    entityManager.flush();
+
+    Comment commentByOtherUser = new Comment(article, commentUser, "다른 사용자가 작성한 댓글 내용 입니다.");
+    commentByOtherUser.update(1L);
+    commentRepository.save(commentByOtherUser);
+    entityManager.flush();
+
+    Like like = new Like(commentByOtherUser, user);
+    likeRepository.save(like);
+    entityManager.flush();
+
+    Interest interest = new Interest(Instant.now(), "스포츠", 1L);
+    interestRepository.save(interest);
+    entityManager.flush();
+
+    Keyword keword1 = new Keyword(Instant.now(), "축구", interest);
+    Keyword keword2 = new Keyword(Instant.now(), "야구", interest);
+    keywordRepository.save(keword1);
+    keywordRepository.save(keword2);
+    entityManager.flush();
+
+    UserInterest userInterest = new UserInterest(Instant.now(), user, interest);
+    userInterestRepository.save(userInterest);
+    entityManager.flush();
+
     entityManager.clear();
 
-    List<SubscriptionDto> subscriptions = new ArrayList<>();
-    subscriptions.add(subscriptionDto);
+    SubscriptionDto subscriptionDto = userInterestMapper.toDto(userInterest);
+    CommentDto commentDto = commentMapper.toDto(comment);
+    CommentLikeDto commentLikeDto = commentMapper.toDto(like);
+    ArticleViewDto articleViewDto = articleViewMapper.toDto(article, user, articleCount);
 
-    List<CommentDto> comments = new ArrayList<>();
-    comments.add(commentDto);
-
-    List<CommentLikeDto> commentLikes = new ArrayList<>();
-    commentLikes.add(commentLikeDto);
-
-    List<ArticleViewDto> articleViews = new ArrayList<>();
-    articleViews.add(articleViewDto);
-
-    given(userActivityService.getUserActivity(user.getId())).willReturn(new UserActivityDto(user.getId(), "test@test.kr", "test", Instant.now(), subscriptions, comments, commentLikes, articleViews));
+    UserActivityDto userActivityDto = new UserActivityDto(
+        user.getId(),
+        "test@test.kr",
+        "test",
+        Instant.now(),
+        List.of(subscriptionDto),
+        List.of(commentDto),
+        List.of(commentLikeDto),
+        List.of(articleViewDto)
+    );
 
     // when and then
-    mockMvc.perform(get("/api/user-activities/" + user.getId()))
+    mockMvc.perform(get("/api/user-activities/{userId}", user.getId()))
         .andExpect(status().isOk())
-        .andExpect(jsonPath("$.userId").value(user.getId().toString()))
+        .andExpect(jsonPath("$.id").value(user.getId().toString()))
         .andExpect(jsonPath("$.email").value("test@test.kr"))
         .andExpect(jsonPath("$.nickname").value("test"))
         .andExpect(jsonPath("$.createdAt").exists())
         .andExpect(jsonPath("$.subscriptions").isArray())
-        .andExpect(jsonPath("$.subscriptions[0].userId").value(user.getId().toString()))
+        .andExpect(jsonPath("$.subscriptions[0].id").value(userInterest.getId().toString()))
         .andExpect(jsonPath("$.subscriptions[0].interestId").value(interest.getId().toString()))
-        .andExpect(jsonPath("$.subscriptions[0].interestType").value("interest"))
-        .andExpect(jsonPath("$.subscriptions[0].keywords[0]").value("축구"))
-        .andExpect(jsonPath("$.subscriptions[0].keywords[1]").value("야구"))
-        .andExpect(jsonPath("$.subscriptions[0].count").value(2L))
+        .andExpect(jsonPath("$.subscriptions[0].interestName").value("스포츠"))
+        .andExpect(jsonPath("$.subscriptions[0].interestKeywords[0]").value("축구"))
+        .andExpect(jsonPath("$.subscriptions[0].interestKeywords[1]").value("야구"))
+        .andExpect(jsonPath("$.subscriptions[0].interestSubscriberCount").value(1L))
         .andExpect(jsonPath("$.subscriptions[0].createdAt").exists())
         .andExpect(jsonPath("$.comments").isArray())
         .andExpect(jsonPath("$.comments[0].id").value(comment.getId().toString()))
@@ -138,35 +167,100 @@ public class UserActivityIntegrationTest {
         .andExpect(jsonPath("$.comments[0].createdAt").exists())
         .andExpect(jsonPath("$.commentLikes").isArray())
         .andExpect(jsonPath("$.commentLikes[0].id").value(like.getId().toString()))
-        .andExpect(jsonPath("$.commentLikes[0].userId").value(user.getId().toString()))
+        .andExpect(jsonPath("$.commentLikes[0].likedBy").value(user.getId().toString()))
         .andExpect(jsonPath("$.commentLikes[0].createdAt").exists())
+        .andExpect(jsonPath("$.commentLikes[0].commentId").value(commentByOtherUser.getId().toString()))
         .andExpect(jsonPath("$.commentLikes[0].articleId").value(article.getId().toString()))
-        .andExpect(jsonPath("$.commentLikes[0].commentId").value(comment.getId().toString()))
-        .andExpect(jsonPath("$.commentLikes[0].commentUserId").value(otherUser.getId().toString()))
+        .andExpect(jsonPath("$.commentLikes[0].commentUserId").value(commentUser.getId().toString()))
         .andExpect(jsonPath("$.commentLikes[0].commentUserNickname").value("commentUser"))
-        .andExpect(jsonPath("$.commentLikes[0].commentUserContent").value("내가 좋아하는 댓글 내용"))
-        .andExpect(jsonPath("$.commentLikes[0].likeCount").isNotEmpty())
-        .andExpect(jsonPath("$.commentLikes[0].likedByMe").value(true))
+        .andExpect(jsonPath("$.commentLikes[0].commentContent").value("다른 사용자가 작성한 댓글 내용 입니다."))
+        .andExpect(jsonPath("$.commentLikes[0].commentLikeCount").value(1L))
+        .andExpect(jsonPath("$.commentLikes[0].commentCreatedAt").exists())
         .andExpect(jsonPath("$.articleViews").isArray())
         .andExpect(jsonPath("$.articleViews[0].id").value(articleCount.getId().toString()))
-        .andExpect(jsonPath("$.articleViews[0].userId").value(user.getId().toString()))
+        .andExpect(jsonPath("$.articleViews[0].viewedBy").value(user.getId().toString()))
         .andExpect(jsonPath("$.articleViews[0].createdAt").exists())
         .andExpect(jsonPath("$.articleViews[0].articleId").value(article.getId().toString()))
         .andExpect(jsonPath("$.articleViews[0].source").value("NAVER"))
-        .andExpect(jsonPath("$.articleViews[0].link").value("http://www.nspna.com/news/?mode=view&newsid=42872"))
-        .andExpect(jsonPath("$.articleViews[0].title").value("뉴스 제목"))
-        .andExpect(jsonPath("$.articleViews[0].createdAt").exists())
+        .andExpect(jsonPath("$.articleViews[0].sourceUrl").value("https://naver.com/news/12333"))
+        .andExpect(jsonPath("$.articleViews[0].title").value("기사제목"))
+        .andExpect(jsonPath("$.articleViews[0].publishDate").exists())
         .andExpect(jsonPath("$.articleViews[0].summary").value("요약"))
-        .andExpect(jsonPath("$.articleViews[0].likeCount").isNotEmpty())
-        .andExpect(jsonPath("$.articleViews[0].viewCount").isNotEmpty());
+        .andExpect(jsonPath("$.articleViews[0].commentCount").value(0L));
   }
 
   @Test
   void 존재하지_않는_사용자_활동_조회_실패() throws Exception {
     // given
-    UUID userId = UUID.randomUUID();
+    User user = User.builder()
+        .email("test@test.kr")
+        .nickname("test")
+        .password("test1234")
+        .isDeleted(false)
+        .build();
+    userRepository.save(user);
+    entityManager.flush();
 
-    mockMvc.perform(get("/api/user-activities/"+userId))
-        .andExpect(status().isBadRequest());
+    Article article = new Article("NAVER", "https://naver.com/news/12333", "기사제목", "요약", Instant.now());
+    articleRepository.save(article);
+    entityManager.flush();
+
+    ArticleCount articleCount = new ArticleCount(article, user);
+    articleCountRepository.save(articleCount);
+    entityManager.flush();
+
+    Comment comment = new Comment(article, user, "댓글 내용 입니다.");
+    commentRepository.save(comment);
+    entityManager.flush();
+
+    User commentUser = User.builder()
+        .email("comment@test.kr")
+        .nickname("commentUser")
+        .password("test1234")
+        .isDeleted(false)
+        .build();
+    userRepository.save(commentUser);
+    entityManager.flush();
+
+    Comment commentByOtherUser = new Comment(article, commentUser, "다른 사용자가 작성한 댓글 내용 입니다.");
+    commentByOtherUser.update(1L);
+    commentRepository.save(commentByOtherUser);
+    entityManager.flush();
+
+    Like like = new Like(commentByOtherUser, user);
+    likeRepository.save(like);
+    entityManager.flush();
+
+    Interest interest = new Interest(Instant.now(), "스포츠", 1L);
+    interestRepository.save(interest);
+    entityManager.flush();
+
+    Keyword keword1 = new Keyword(Instant.now(), "축구", interest);
+    Keyword keword2 = new Keyword(Instant.now(), "야구", interest);
+    keywordRepository.save(keword1);
+    keywordRepository.save(keword2);
+    entityManager.flush();
+
+    UserInterest userInterest = new UserInterest(Instant.now(), user, interest);
+    userInterestRepository.save(userInterest);
+    entityManager.flush();
+
+    entityManager.clear();
+
+    SubscriptionDto subscriptionDto = userInterestMapper.toDto(userInterest);
+    CommentDto commentDto = commentMapper.toDto(comment);
+    CommentLikeDto commentLikeDto = commentMapper.toDto(like);
+    ArticleViewDto articleViewDto = articleViewMapper.toDto(article, user, articleCount);
+
+    UserActivityDto userActivityDto = new UserActivityDto(
+        user.getId(),
+        "test@test.kr",
+        "test",
+        Instant.now(),
+        List.of(subscriptionDto),
+        List.of(commentDto),
+        List.of(commentLikeDto),
+        List.of(articleViewDto)
+    );
   }
 }
