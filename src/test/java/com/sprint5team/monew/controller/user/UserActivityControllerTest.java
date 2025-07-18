3619d40ee43d132321ea0551242a431e348203c1
JinsolPark <jinsol8k@gmail.com>
2025-07-18 11:55:18 +0900

    feat: 사용자 활동 내역 조회 Controller 구현(green)

--- src/test/java/com/sprint5team/monew/controller/user/UserActivityControllerTest.java
+++ src/test/java/com/sprint5team/monew/controller/user/UserActivityControllerTest.java
@@ -13,7 +13,6 @@ import com.sprint5team.monew.domain.user.dto.UserActivityDto;
 import com.sprint5team.monew.domain.user.service.UserActivityServiceImpl;
 import com.sprint5team.monew.domain.user_interest.dto.SubscriptionDto;
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
@@ -42,45 +41,41 @@ public class UserActivityControllerTest {
     UUID commentLikeId = UUID.randomUUID();
     UUID commentUserId = UUID.randomUUID();
 
-    List<String> keywords = new ArrayList<>();
-    keywords.add("축구");
-    keywords.add("야구");
-    keywords.add("농구");
+    List<String> keywords = List.of("축구", "야구", "농구");
 
     SubscriptionDto subscriptionDto = new SubscriptionDto(userId, interestId,"interest", keywords, 1L, Instant.now());
     CommentDto commentDto = new CommentDto(commentId, articleId, userId, "test", "댓글 내용 입니다.",0L, false, Instant.now());
     CommentLikeDto commentLikeDto = new CommentLikeDto(commentLikeId, userId, Instant.now(), commentId, articleId, commentUserId, "commentUser", "내가 좋아하는 댓글 내용", 1L, Instant.now());
-    ArticleViewDto articleViewDto = new ArticleViewDto(articleViewId, userId, Instant.now(), articleId, "NAVER", "https://naver.com/news/12333", "test", Instant.now(), "요약", 10, 10);
+    ArticleViewDto articleViewDto = new ArticleViewDto(articleViewId, userId, Instant.now(), articleId, "NAVER", "https://naver.com/news/12333", "test", Instant.now(), "요약", 10L, 10L);
 
-    List<SubscriptionDto> subscriptions = new ArrayList<>();
-    subscriptions.add(subscriptionDto);
+    UserActivityDto userActivityDto = new UserActivityDto(
+        userId,
+        "test@test.kr",
+        "test",
+        Instant.now(),
+        List.of(subscriptionDto),
+        List.of(commentDto),
+        List.of(commentLikeDto),
+        List.of(articleViewDto)
+    );
 
-    List<CommentDto> comments = new ArrayList<>();
-    comments.add(commentDto);
-
-    List<CommentLikeDto> commentLikes = new ArrayList<>();
-    commentLikes.add(commentLikeDto);
-
-    List<ArticleViewDto> articleViews = new ArrayList<>();
-    articleViews.add(articleViewDto);
-
-    given(userActivityService.getUserActivity(userId)).willReturn(new UserActivityDto(userId, "test@test.kr", "test", Instant.now(), subscriptions, comments, commentLikes, articleViews));
+    given(userActivityService.getUserActivity(userId)).willReturn(userActivityDto);
 
     // when and then
     mockMvc.perform(get("/api/user-activities/{userId}", userId))
         .andExpect(status().isOk())
-        .andExpect(jsonPath("$.userId").value(userId.toString()))
+        .andExpect(jsonPath("$.id").value(userId.toString()))
         .andExpect(jsonPath("$.email").value("test@test.kr"))
         .andExpect(jsonPath("$.nickname").value("test"))
         .andExpect(jsonPath("$.createdAt").exists())
         .andExpect(jsonPath("$.subscriptions").isArray())
-        .andExpect(jsonPath("$.subscriptions[0].userId").value(userId.toString()))
+        .andExpect(jsonPath("$.subscriptions[0].id").value(userId.toString()))
         .andExpect(jsonPath("$.subscriptions[0].interestId").value(interestId.toString()))
-        .andExpect(jsonPath("$.subscriptions[0].interestType").value("interest"))
-        .andExpect(jsonPath("$.subscriptions[0].keywords[0]").value("축구"))
-        .andExpect(jsonPath("$.subscriptions[0].keywords[1]").value("야구"))
-        .andExpect(jsonPath("$.subscriptions[0].keywords[2]").value("농구"))
-        .andExpect(jsonPath("$.subscriptions[0].count").value(1L))
+        .andExpect(jsonPath("$.subscriptions[0].interestName").value("interest"))
+        .andExpect(jsonPath("$.subscriptions[0].interestKeywords[0]").value("축구"))
+        .andExpect(jsonPath("$.subscriptions[0].interestKeywords[1]").value("야구"))
+        .andExpect(jsonPath("$.subscriptions[0].interestKeywords[2]").value("농구"))
+        .andExpect(jsonPath("$.subscriptions[0].interestSubscriberCount").value(1L))
         .andExpect(jsonPath("$.subscriptions[0].createdAt").exists())
         .andExpect(jsonPath("$.comments").isArray())
         .andExpect(jsonPath("$.comments[0].id").value(commentId.toString()))
@@ -93,26 +88,26 @@ public class UserActivityControllerTest {
         .andExpect(jsonPath("$.comments[0].createdAt").exists())
         .andExpect(jsonPath("$.commentLikes").isArray())
         .andExpect(jsonPath("$.commentLikes[0].id").value(commentLikeId.toString()))
-        .andExpect(jsonPath("$.commentLikes[0].userId").value(userId.toString()))
+        .andExpect(jsonPath("$.commentLikes[0].likedBy").value(userId.toString()))
         .andExpect(jsonPath("$.commentLikes[0].createdAt").exists())
-        .andExpect(jsonPath("$.commentLikes[0].articleId").value(articleId.toString()))
         .andExpect(jsonPath("$.commentLikes[0].commentId").value(commentId.toString()))
+        .andExpect(jsonPath("$.commentLikes[0].articleId").value(articleId.toString()))
         .andExpect(jsonPath("$.commentLikes[0].commentUserId").value(commentUserId.toString()))
         .andExpect(jsonPath("$.commentLikes[0].commentUserNickname").value("commentUser"))
-        .andExpect(jsonPath("$.commentLikes[0].commentUserContent").value("내가 좋아하는 댓글 내용"))
-        .andExpect(jsonPath("$.commentLikes[0].likeCount").value(1L))
-        .andExpect(jsonPath("$.commentLikes[0].likedByMe").value(true))
+        .andExpect(jsonPath("$.commentLikes[0].commentContent").value("내가 좋아하는 댓글 내용"))
+        .andExpect(jsonPath("$.commentLikes[0].commentLikeCount").value(1L))
+        .andExpect(jsonPath("$.commentLikes[0].commentCreatedAt").exists())
         .andExpect(jsonPath("$.articleViews").isArray())
         .andExpect(jsonPath("$.articleViews[0].id").value(articleViewId.toString()))
-        .andExpect(jsonPath("$.articleViews[0].userId").value(userId.toString()))
+        .andExpect(jsonPath("$.articleViews[0].viewedBy").value(userId.toString()))
         .andExpect(jsonPath("$.articleViews[0].createdAt").exists())
         .andExpect(jsonPath("$.articleViews[0].articleId").value(articleId.toString()))
         .andExpect(jsonPath("$.articleViews[0].source").value("NAVER"))
-        .andExpect(jsonPath("$.articleViews[0].link").value("https://naver.com/news/12333"))
+        .andExpect(jsonPath("$.articleViews[0].sourceUrl").value("https://naver.com/news/12333"))
         .andExpect(jsonPath("$.articleViews[0].title").value("test"))
-        .andExpect(jsonPath("$.articleViews[0].createdAt").exists())
+        .andExpect(jsonPath("$.articleViews[0].publishDate").exists())
         .andExpect(jsonPath("$.articleViews[0].summary").value("요약"))
-        .andExpect(jsonPath("$.articleViews[0].likeCount").value(10))
-        .andExpect(jsonPath("$.articleViews[0].viewCount").value(10));
+        .andExpect(jsonPath("$.articleViews[0].commentCount").value(10L))
+        .andExpect(jsonPath("$.articleViews[0].viewCount").value(10L));
   }
 }
