0848fb9c3502b6a557b61444c7ce57e6c29f91e8
Kangmoongu <sprtms5325@naver.com>
2025-07-22 09:23:30 +0900

    refactor: Comment의 BaseException 추가하여 리팩토링 및 AlreadyLikedException 추가

---
+++ src/main/java/com/sprint5team/monew/domain/comment/exception/AlreadyLikedException.java
@@ -0,0 +1,20 @@
+package com.sprint5team.monew.domain.comment.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class AlreadyLikedException extends CommentException {
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return HttpStatus.CONFLICT;
+    }
+
+    @Override
+    public String getMessage() {return "이미 좋아요를 누른 댓글입니다.";}
+
+    @Override
+    public String getDetails() {
+        return "";
+    }
+}
+
